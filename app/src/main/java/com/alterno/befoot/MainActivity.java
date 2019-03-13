658478b24a5c89fb1a212f299f1f05700e10552e
Alessandro Alterno <alternonolimit@gmail.com>
2019-03-13 15:10:18 +0100

    Modified navigation names and added a sample to generate matches list

--- app/src/main/java/com/alterno/befoot/MainActivity.java
+++ app/src/main/java/com/alterno/befoot/MainActivity.java
@@ -5,11 +5,17 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     private TextView mTextMessage;
+    private ListView listMatches;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -17,14 +23,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_matches:
+                    //mTextMessage.setText(R.string.title_matches);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_leagues:
+                    //mTextMessage.setText(R.string.title_leagues);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_teams:
+                    //mTextMessage.setText(R.string.title_teams);
                     return true;
             }
             return false;
@@ -35,10 +41,28 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //mTextMessage = (TextView) findViewById(R.id.message);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+        listMatches = (ListView) findViewById(R.id.listMatches);
+
+        List<Match> matches = genererMatch();
+
+        MatchAdapter adapter = new MatchAdapter(MainActivity.this,matches);
+        listMatches.setAdapter(adapter);
+
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    // TODO : Remplacer le core de la méthode par les appels API
+    private List<Match> genererMatch(){
+        List<Match> matches = new ArrayList<>();
+
+        matches.add(new Match("Dijon","PSG","0","4"));
+        matches.add(new Match("Juventus","Atletico Madrid","3","0"));
+        matches.add(new Match("Manchester City","Schalke 04","7","0"));
+
+        return matches;
     }
 
 }
